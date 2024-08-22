19db201f453d07906475fd4f475af053582f2ede
lerderl <josepholukunle1107@gmail.com>
2024-08-22 06:47:21 +0100

    feat: add routing

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import "../App.css";
+import logo from "../logo.svg";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        WELCOME TO MY PROTFOLIO
+      </header>
+    </div>
+  );
+}
+
+export default Home;
